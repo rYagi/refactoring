c781ee40843168e276483f98cb754253145bbf01
rYagi <r-yagi565@live.jp>
2020-10-20 05:59:06 +0900

    P22
    変数名の変更result

--- first.mjs
+++ first.mjs
@@ -47,19 +47,19 @@ export function statement(invoice, plays){
 	}
 
 	function totalVolumeCredits(){
-		let volumeCredits =0;
+		let result =0;
 		for (let perf of invoice.performances) {
-			volumeCredits += volumeCreditsFor(perf);
+			result += volumeCreditsFor(perf);
 		}
-		return volumeCredits;
+		return result;
 	}
 
 	function totalAmount(){
-		let totalAmount = 0;
+		let result = 0;
 		for (let perf of invoice.performances) {
-			totalAmount += amountFor(perf);
+			result += amountFor(perf);
 		}
-		return totalAmount;	
+		return result;	
 	}
 }
 
